257a4c02d4018acfc09966af3df6e3ef556ee338
jasonstanton <7691254@student.swin.edu.au>
2017-04-06 00:27:10 +1000

    Update login.js

--- login.js
+++ login.js
@@ -4,4 +4,10 @@ function validate()
 {
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
+	
+	sessionStorage.username = username;
+	sessionStorage.password = password;
+	
+	var json = {
+	};
 }
