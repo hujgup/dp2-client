c9a0e649f48fae4032ec9517674a34a40b76c8b4
Jason Stanton <Jason Stanton>
2017-04-09 23:19:44 +1000

    Added functionality to add a sales record

--- addsales.js
+++ addsales.js
@@ -1,12 +1,24 @@
 "use strict";
 
-function insertData() {	
+function getCurrentDate() {
 	var date = new Date();
-	
+	date = date.toISOString().replace(/[^0-9a-zA-Z]/g,'');	// current date+time ISO 8601 
+	date = date.substring(0,15) + date.substring(18,19); // remove extra characters
+	return date;
+}
+
+function getDate() {
+	var date = document.getElementById("datetime");
+	if(document.getElementBy)
+		
+	//Regex for date (\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2})$
+}
+
+function insertData(event) {	
 	var json = {
 		"authent": {
-			"username": "Test",
-			"password": "password"
+			"username": "feferi",
+			"password": "0413"
 		},
 		
 		"requests": [
@@ -14,12 +26,9 @@ function insertData() {
 				"type": "add",
 				"records": [
 					{
-						//"product": document.getElementById("product").value,
-						//"quantity": document.getElementById("quantity").value,
-						//"dateTime": date.toISOString().replace(/[^0-9a-zA-Z]/g,'')	// current date+time ISO 8601 
-						"product": 5,
-						"quantity": 1,
-						"dateTime": "20170328T123456Z"
+						"product": Number(document.getElementById("product").value),
+						"quantity": Number(document.getElementById("quantity").value),
+						"dateTime": getCurrentDate()
 					}
 				]
 			}
@@ -29,11 +38,24 @@ function insertData() {
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", "../dp2-api/api/", true);	// All requests should be of type post
 	xhr.onreadystatechange = function() {
-		if(xhr.readyState == 4 && this.status == 200) {
-			console.log(JSON.parse(xhr.responseText)); // Barring error conditions, responses will be valid JSON
+		if(xhr.readyState == 4) {
+			if(xhr.status == 200) {
+				console.log(JSON.parse(xhr.responseText)); // Barring error conditions, responses will be valid JSON
+			} else {
+				console.error(xhr.responseText);
+			}
 		}
 	};
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	xhr.send("request=" + encodeURIComponent(JSON.stringify(json)));	// Request JSON should be set to the request key
+	
+	event.returnValue = false;
+	return false;
 }
 
+function init()
+{
+	document.getElementById("addsalesform").onsubmit = insertData;
+}
+
+window.onload = init;
